9d191281284fb1d384a2435cbd9455f26a917ff3
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-21 19:38:15 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :costumes do |t|
+      t.string :name 
+      t.integer :price
+      t.string :image_url 
+      t.string :size 
+      t.timestamps 
+    end 
+  end 
+end
