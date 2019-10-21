37756f3cab001fad04db132b33ff14d466961417
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-21 20:03:50 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime opening_time
+      t.datetime :start_time
       t.datetime :end_time
     end 
   end 
